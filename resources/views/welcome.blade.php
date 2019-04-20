6e64742ea567efcaebd5b4a9290f8afa3f965e80
GuilhermeHenrique456 <47119596+GuilhermeHenrique456@users.noreply.github.com>
2019-04-20 15:18:30 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    Testes
                 </div>
 
                 <div class="links">
